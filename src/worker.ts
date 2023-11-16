3db722bfb3f78f02c143dc5039a8d1157d5bb0e6
sportshead <me@sportshead.dev>
2023-11-16 21:00:12 +0000

    respond with json instead of html

--- src/worker.ts
+++ src/worker.ts
@@ -1,62 +1,67 @@
-import { Context as HonoContext, Env as HonoEnv, Hono } from 'hono'
+import { Context as HonoContext, Env as HonoEnv, Hono } from "hono";
 
 type Bindings = {
-	R2: R2Bucket,
-	PUBLIC_BUCKET_URL: string
-}
+    R2: R2Bucket;
+    PUBLIC_BUCKET_URL: string;
+};
 
 // html.d.ts tells typescript that this is a normal thing to do
-import indexHtml from './index.html'
-import uploadHtml from './upload.html'
+import indexHtml from "./index.html";
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('*', async (c) => {
-	return c.html(indexHtml)
-})
+app.get("*", async (c) => {
+    return c.html(indexHtml);
+});
 
-app.post('/upload', async (c) => {
-	const formdata = await c.req.formData()
-	const file = formdata.get('file');
+app.post("/upload", async (c) => {
+    const formdata = await c.req.formData();
+    const file = formdata.get("file");
 
-	let fileName = (Math.random() + 1).toString(36).substring(5);
+    let fileName = (Math.random() + 1).toString(36).substring(5);
 
-	if (file instanceof File) {
-		const extension = file.name.split('.').pop();
-		fileName = fileName + (extension ? '.' + extension : '')
+    if (file instanceof File) {
+        const extension = file.name.split(".").pop();
+        fileName = fileName + (extension ? "." + extension : "");
 
-		await c.env.R2.put(fileName, file.stream(), {httpMetadata: {contentType: file.type}})
+        await c.env.R2.put(fileName, file.stream(), {
+            httpMetadata: { contentType: file.type },
+        });
 
-		const html = uploadHtml.replaceAll('__URLGOESHERE__', c.env.PUBLIC_BUCKET_URL + '/' + fileName)
-		return c.html(html)
-	}
-})
+        return c.json({ url: c.env.PUBLIC_BUCKET_URL + "/" + fileName });
+    }
+});
 
-app.post('/url', async (c) => {
-	const formdata = await c.req.formData()
-	const urlString = formdata.get('url');
+app.post("/url", async (c) => {
+    const formdata = await c.req.formData();
+    const urlString = formdata.get("url");
 
-	let fileName = (Math.random() + 1).toString(36).substring(5);
+    let fileName = (Math.random() + 1).toString(36).substring(5);
 
-	if (urlString !== null) {
-		const url = new URL(urlString.toString())
+    if (urlString !== null) {
+        const url = new URL(urlString.toString());
 
-		const resp = await fetch(url)
+        const resp = await fetch(url);
 
-		if (!resp.ok){
-			return c.text("your url returned non-ok status code: " + resp.status)
-		}
+        if (!resp.ok) {
+            return c.text(
+                "your url returned non-ok status code: " + resp.status,
+            );
+        }
 
-		const extension = url.pathname.split('.').pop();
-		fileName = fileName + (extension ? '.' + extension : '')
+        const extension = url.pathname.split(".").pop();
+        fileName = fileName + (extension ? "." + extension : "");
 
-		await c.env.R2.put(fileName, resp.body, {httpMetadata: {contentType: resp.headers.get('content-type')?.toString()}})
+        await c.env.R2.put(fileName, resp.body, {
+            httpMetadata: {
+                contentType: resp.headers.get("content-type")?.toString(),
+            },
+        });
 
-		const html = uploadHtml.replaceAll('__URLGOESHERE__', c.env.PUBLIC_BUCKET_URL + '/' + fileName)
-		return c.html(html)
-	}
+        return c.json({ url: c.env.PUBLIC_BUCKET_URL + "/" + fileName });
+    }
 
-	return c.text("something went wrong in your input")
-})
+    return c.text("something went wrong in your input");
+});
 
-export default app
+export default app;
